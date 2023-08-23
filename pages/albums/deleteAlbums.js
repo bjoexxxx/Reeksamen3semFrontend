b82c1c45753bb5390e90b75a00f8814e94681331
bjoexxxx <bjoe6702@stud.kea.dk>
2023-08-23 19:25:05 +0200

    delete function working, but returning error

--- pages/albums/deleteAlbums.js
+++ pages/albums/deleteAlbums.js
@@ -1,5 +1,56 @@
 import { handleHttpErrors, makeOptions } from "../../utils.js";
 
 export function initDeleteAlbums(){
-    deleteAlbums()
+    makeHTML();
+}
+
+async function deleteAlbums(id) {
+
+    const options = makeOptions("DELETE", "", false);
+
+    try {
+        const response = await fetch("http://localhost:8080/api/album/" + id, options)
+            .then(handleHttpErrors);
+
+        if(response.ok) {
+            alert('Album deleted successfully!');
+        } else {
+            alert('Error deleting album.');
+        }
+
+    } catch (error) {
+        console.log(error);
+        alert('Error deleting album. Please try again.');
+    }
+}
+
+
+function makeHTML() {
+    const content = document.getElementById("content");
+
+
+    const form = `
+        <form id="deleteAlbumForm">
+            <div class="form-group">
+                <label for="albumId">Album ID:</label>
+                <input type="text" class="form-control" id="albumId" required>
+            </div>
+            <button type="submit" class="btn btn-danger">Delete Album</button>
+        </form>
+    `;
+
+    content.innerHTML = form;
+
+    document.getElementById("deleteAlbumForm").addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        const albumId = document.getElementById("albumId").value;
+
+        if(albumId) {
+            await deleteAlbums(albumId);
+        } else {
+            alert('Please enter a valid album ID.');
+        }
+    });
+
 }
